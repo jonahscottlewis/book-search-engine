3d74de974a34bd6bfb8536abc695536b50eafdc4
Jonah Lewis <jonahslewis96@gmail.com>
2023-05-18 21:59:31 -0700

    added to pages

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,6 +7,9 @@ import {
   Col
 } from 'react-bootstrap';
 
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,6 +8,8 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -20,6 +22,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
